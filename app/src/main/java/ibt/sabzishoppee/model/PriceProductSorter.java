eff69d40600e648042fd0896cd2123467310445c
amar <amardeep.infobite@gmail.com>
2019-04-02 10:20:42 +0530

    sabzi

---
+++ app/src/main/java/ibt/sabzishoppee/model/PriceProductSorter.java
@@ -0,0 +1,30 @@
+package ibt.sabzishoppee.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import ibt.sabzishoppee.model.productlist_responce.Product;
+
+public class PriceProductSorter {
+
+    ArrayList<Product> jobCandidate = new ArrayList<>();
+    public PriceProductSorter(ArrayList<Product> jobCandidate) {
+        this.jobCandidate = jobCandidate;
+    }
+    public ArrayList<Product> getSortedJobCandidateByAge() {
+        Collections.sort(jobCandidate);
+        return jobCandidate;
+    }
+
+    public ArrayList<Product> getSortedJobCandidateByName() {
+        Collections.sort(jobCandidate, Product.nameComparator);
+        return jobCandidate;
+    }
+
+
+    public ArrayList<Product> getSortedProductByHightoLow() {
+        Collections.sort(jobCandidate, Product.hightolowComparator);
+        return jobCandidate;
+    }
+
+}
