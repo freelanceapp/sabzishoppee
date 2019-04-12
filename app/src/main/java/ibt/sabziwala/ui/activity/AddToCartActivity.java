486f8c5fb3a690b0e7a032af37a480b0ea969c8a
amar <amardeep.infobite@gmail.com>
2019-04-12 11:42:22 +0530

    scknckn

--- app/src/main/java/ibt/sabziwala/ui/activity/AddToCartActivity.java
+++ app/src/main/java/ibt/sabziwala/ui/activity/AddToCartActivity.java
@@ -204,7 +204,7 @@ public class AddToCartActivity extends BaseActivity implements View.OnClickListe
         if (qty == minQty) {
             databaseCart.deleteContact(productDetail);
             startActivity(new Intent(mContext, AddToCartActivity.class));
-            finish();
+           // finish();
         } else {
             qty--;
             productDetail.setQuantity(qty);
@@ -255,7 +255,7 @@ public class AddToCartActivity extends BaseActivity implements View.OnClickListe
             AppPreference.setStringPreference(ctx, Constant.TOTAL_AMOUNT, String.valueOf(total));
             if (cartlist.size() > 0) {
                 startActivity(new Intent(ctx, CheckOutActivity.class));
-                finish();
+                //finish();
             }
         }
     }
